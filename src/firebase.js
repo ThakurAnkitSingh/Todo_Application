bee9c841b969cc1f1b476243fa3d07129a8e16b9
ThakurAnkitSingh <akashthakur70423@gmail.com>
2023-05-03 22:19:37 +0530

    Todo Second  commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import 'firebase/compat/firestore';
 
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBHW904Ej5pSVvgy0V-O-a4-k1B6Y1f6s8",
-    authDomain: "todo-app-8fdac.firebaseapp.com",
-    projectId: "todo-app-8fdac",
-    storageBucket: "todo-app-8fdac.appspot.com",
-    messagingSenderId: "980579108650",
-    appId: "1:980579108650:web:c84b09e4c4cffb9c87c053",
-    measurementId: "G-D2K30GQWQB"
+    
 });
 
 
